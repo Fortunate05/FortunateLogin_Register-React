03c1f0afd115632259863d6741d2c6b40ffef9d7
unknown <you@example.com>
2023-04-05 13:09:14 +0200

    add code copy for create

--- Ntwanano/src/main.jsx
+++ Ntwanano/src/main.jsx
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+    
+  },
+  {
+    path: "/Register",
+    element: <Register />,
+    
+  },
+  
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <RouterProvider router={router} />
