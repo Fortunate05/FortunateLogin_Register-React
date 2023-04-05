e55b401a92167b3cc6509adce1e27b2ece370d06
unknown <you@example.com>
2023-04-05 10:04:54 +0200

    main.jsx do add copy google from react router

--- Fortunate/src/main.jsx
+++ Fortunate/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import Home from './Pages/Home'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Register from './Pages/Register';
+import Login from '/Pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <Register />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
