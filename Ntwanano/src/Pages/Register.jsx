73242c349d5ddf7bed966bb907bbc6a6168b6ddd
unknown <you@example.com>
2023-04-06 11:23:05 +0200

    added code from react bootstraps  in register.jsx

--- Ntwanano/src/Pages/Register.jsx
+++ Ntwanano/src/Pages/Register.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import Header from '../Components/Header'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Register() {
   return (
-    <div><Header/>
-    
-    
-    
+    <div><Header />
+      <Form>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="Username" placeholder="Enter Username" />
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="Confirm" placeholder="Password" />
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   )
 }
